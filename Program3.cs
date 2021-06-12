42cc8e0337177a7d111ccb40d4201fc6470c0eaf
ByGonzalo <84694563+ByGonzalo@users.noreply.github.com>
2021-06-12 12:05:52 -0500

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread pares = new System.Threading.Thread(new System.Threading.ThreadStart(NumerosPares));
+            System.Threading.Thread inpares = new System.Threading.Thread(new System.Threading.ThreadStart(NumerosInpares));
+            pares.Start();
+            inpares.Start();
+            Console.ReadKey();
+        }
+        static void NumerosPares()
+        {           
+            for(int i = 1; i <= 100000; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    Console.WriteLine("P "+i);
+                }
+            }
+        }
+        static void NumerosInpares()
+        {       
+            for(int i = 1; i <= 100000; i++)
+            {
+                if ((i % 2) != 0)
+                {
+                    Console.WriteLine("I "+i);
+                }
+            }
+        }
+    }
+}
